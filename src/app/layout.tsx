f9d8494bdaddedd173fed1a2eecbc7612e51f623
Hadaya5 <hadaya456@gmail.com>
2025-10-13 00:25:48 -0400

    Add favicon and manifest config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Footer, Navbar } from "@/components";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Baila Ciencias" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,10 +7,10 @@ export function Footer() {
   return (
     <footer className="py-4 px-4 border-t border-border">
       <div className="container mx-auto text-center">
-        <div className="flex  justify-center">
+        <div className="flex justify-center">
           <Logo />
         </div>
-        <p className="text-muted-foreground">
+        <p className="text-muted-foreground mt-1">
           © {currentDate.getFullYear()} Baila Ciencias. Transformando la
           educación a través del movimiento y la creatividad.
         </p>
